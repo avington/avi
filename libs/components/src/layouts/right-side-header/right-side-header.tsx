3ffdf60350d3950d211aa8a82d9af41ea183e607
Steven Moseley <smoseley@anthology.com>
2024-08-30 17:29:18 -0400

    initial commit

---
+++ libs/components/src/layouts/right-side-header/right-side-header.tsx
@@ -0,0 +1,11 @@
+import styles from './right-side-header.module.scss';
+
+export function RightSideHeader() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to RightSideHeader!</h1>
+    </div>
+  );
+}
+
+export default RightSideHeader;
