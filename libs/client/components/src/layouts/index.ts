f1b6094423cf69bd349145332c455cb0021e4f25
Steven Moseley <smoseley@anthology.com>
2024-09-10 17:20:39 -0400

    work on the summary portfolio panel

--- libs/client/components/src/layouts/index.ts
+++ libs/client/components/src/layouts/index.ts
@@ -3,3 +3,4 @@ export * from './left-side-header/left-side-header';
 export * from './right-side-header/right-side-header';
 export * from './outer-chrome/outer-chrome';
 export * from './view-container/view-container';
+export * from './large-panel/large-panel';
--- libs/client/features/portfolios/src/components/portfolios/portfolios-summary/portfolios-summary.tsx
+++ libs/client/features/portfolios/src/components/portfolios/portfolios-summary/portfolios-summary.tsx
@@ -1,10 +1,33 @@
+import { LargePanel } from '@avi/client-components';
 import styles from './portfolios-summary.module.scss';
 
 export function PortfoliosSummary() {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to PortfoliosSummary!</h1>
-    </div>
+    <LargePanel className={styles['container']} style={{ width: '1280px' }}>
+      <div className={styles['total']}>
+        <div className={styles['cell-heading']}>All Portfolio Holdings</div>
+        <div className={styles['total-number']}>$600,000</div>
+      </div>
+      <div className={styles['new']}>
+        <button>New Portfolio</button>
+      </div>
+      <div className={styles['cash']}>
+        <div className={styles['cell-heading']}>Cash</div>
+        <div>$1,000</div>
+      </div>
+      <div className={styles['day-change']}>
+        <div className={styles['cell-heading']}>Day Change</div>
+        <div>$5,000 (5.0%)</div>
+      </div>
+      <div className={styles['unrealized']}>
+        <div className={styles['cell-heading']}>Unrealized Gains</div>
+        <div>$50,000 (25.0%)</div>
+      </div>
+      <div className={styles['realized']}>
+        <div className={styles['cell-heading']}>Realized Gains</div>
+        <div>$5,000 (5.0%)</div>
+      </div>
+    </LargePanel>
   );
 }
 
